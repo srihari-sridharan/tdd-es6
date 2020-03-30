fce1dd3ecb71b0d183caa48f58fd3ac811dbc00e
Craft Your Code <craftyourcode@gmail.com>
2020-03-30 12:24:18 +0530

    Green! Passing test.

--- integration-tests/src/server.js
+++ integration-tests/src/server.js
@@ -1,9 +1,11 @@
 import express from "express";
+import db from "./db";
 
 const app = express();
 
 app.get("/users/:username", async (req, res) => {
-  res.json({});
+  const { username } = req.params;
+  res.json(await db.getUserByUsername(username));
 });
 
 export { app };
